ec3e732dd94293dc2cf697f3badb0f130ae95343
Adam Estela <adamestela@gmail.com>
2014-06-21 20:37:10 -0700

    click for full text

--- js/reviewPost.js
+++ js/reviewPost.js
@@ -1,8 +1,20 @@
 // THIS FILE IS RESPONSIBLE FOR THE FRONT END DYNAMIC QUALITY OF A REVIEW POST
 
-// Display the full text when 'more' is selected.
+// GOAL: Animate growth of text 
+//		 Use auto to scale height
 $(document).ready(function(){
-	$('.moreText').click(function(){
-		$('reviewText').css('max-height', 'auto');
+	$reviewText = $('.reviewText');
+
+	$reviewText.click(function(){
+		if($reviewText.css('max-height') === '200px'){
+			$reviewText.removeClass('gradientOverlay');
+			//$reviewText.css('max-height', 'auto');
+			$reviewText.css('max-height', '500px');
+			//$reviewText.animate({height:'500px'}, 500);
+		}
+		else{
+			$reviewText.addClass('gradientOverlay');
+			$reviewText.css('max-height', '200px');
+		}
 	});
 });
